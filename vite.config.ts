f876855d2827391e509848170f8740d371e58135
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-17 19:44:18 +0100

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      input: 'src/main.tsx', // Adjust this to your entry file
+    },
+  },
+};
