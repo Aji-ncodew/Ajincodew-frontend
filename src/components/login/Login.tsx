01d4f068e206b074b36dcccdeb5878b887f0398c
Zakia-Regoug <regougzakia@gmail.com>
2024-02-13 22:26:11 +0100

    validation login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,11 +4,73 @@ import { faGooglePlusG, faFacebookF, faGithub, faLinkedinIn } from '@fortawesome
 import './Login.css';
 
 const Login: React.FC = () => {
+
   const [isContainerActive, setContainerActive] = useState(false);
 
   const handleToggleClick = () => {
     setContainerActive(!isContainerActive);
   };
+
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateEmail = (email : any) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+  
+  const validatePassword = (password : any) => {
+    // Exige au moins 8 caractères, au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
+  
+  const handleEmailChange = (e:any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e:any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setEmailError('Veuillez saisir une adresse email valide.');
+      return;
+    } else {
+      setEmailError('');
+    }
+
+    if (!validatePassword(password)) {
+      setPasswordError('Le mot de passe doit comporter au moins 8 caractères, une minuscule, une majuscule, un chiffre et un caractère spécial.');
+      return;
+    } else {
+      setPasswordError('');
+    }
+
+    console.log("email : "+email+" password : "+password);
+  };
+
+  // const handleLogin = async(e) => {
+  //   e.preventDefault()
+  //  axios.post('/api/login', {
+  //   email: email,
+  //   password : password,
+  // })  
+  // .then(response => {
+  //   if (response.status === 201) {
+  //     router.push('/admin/chemDashboard');
+  //   }else {
+  //     throw new Error('Request failed with status ' + response.status);
+  //   }
+  // });
+    
+    
 
   return (
     <>
@@ -19,7 +81,7 @@ const Login: React.FC = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <div id="auth" className={`container ${isContainerActive ? 'active' : ''}`}>
         <div className={`form-container ${isContainerActive ? 'sign-in' : 'sign-up'}`}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1>{isContainerActive ? 'Sign In' : 'Create Account'}</h1>
             <div className="social-icons">
               <a href="#" className="icon"><FontAwesomeIcon icon={faGooglePlusG} /></a>
@@ -29,9 +91,16 @@ const Login: React.FC = () => {
             </div>
             {!isContainerActive && <span>or use your email for registration</span>}
             {!isContainerActive && <input type="text" placeholder="Name" />}
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>{isContainerActive ? 'Sign In' : 'Sign Up'}</button>
+
+
+            <input type="email" placeholder="Email" value={email} onChange={handleEmailChange}/>
+            {emailError && <span style={{ color: 'red' }}>{emailError}</span>}
+
+
+            <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>
+            {passwordError && <span style={{ color: 'red' }}>{passwordError}</span>}
+
+            <button type='submit'>{isContainerActive ? 'Sign In' : 'Sign Up'}</button>
           </form>
         </div>
 
