d7161d0a9750060fff1a69a8bc30bb3bc7469eac
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-08-22 17:47:30 +0200

    'new'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+      unoptimized: true,  // Disable image optimization for static export
+    },
   };
   
   module.exports = nextConfig;
