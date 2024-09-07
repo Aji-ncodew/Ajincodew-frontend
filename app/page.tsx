ccde3327411c6f86ab91b09dcfda68c30b1a4000
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-09-08 01:47:00 +0200

    add adsence script

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+import Head from 'next/head';
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
 
 export default function Home() {
   return (
-    <main className="h-full w-full">
-      <div className="flex flex-col gap-20">
-        <Hero />
-        <Skills />
-      </div>
-    </main>
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5072302374460982"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <main className="h-full w-full">
+        <div className="flex flex-col gap-20">
+          <Hero />
+          <Skills />
+        </div>
+      </main>
+    </>
   );
 }
